95af7610890a54e443b9ec2b15523582b6e235cf
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-22 14:18:51 +0100

    feat: add custom plugin to include cast-shim reference in index.d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import fs from 'fs' // Added fs import
 import path from 'path'
+import copy from 'rollup-plugin-copy'
 import del from 'rollup-plugin-delete'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
-import copy from 'rollup-plugin-copy'
 import packageJson from './package.json' with { type: 'json' }
 
 const outputDirectory = path.parse(packageJson.main).dir
+
+// Custom plugin to add reference to cast-shim.d.ts
+const addCastShimReference = () => ({
+	name: 'add-cast-shim-reference',
+	writeBundle: async () => {
+		const dtsPath = path.resolve(outputDirectory, 'index.d.ts')
+		const shimReference = '/// <reference path="./types/cast-shim.d.ts" />\n'
+
+		if (fs.existsSync(dtsPath)) {
+			const content = fs.readFileSync(dtsPath, 'utf8')
+			if (!content.startsWith(shimReference)) {
+				fs.writeFileSync(dtsPath, shimReference + content, 'utf8')
+			}
+		}
+	},
+})
 
 export default {
 	input: './src/index.ts',
@@ -15,7 +32,6 @@ export default {
 		dir: outputDirectory,
 		format: 'esm',
 		sourcemap: true,
-		preserveModules: true,
 	},
 	external: [
 		'react',
@@ -33,5 +49,6 @@ export default {
 		copy({
 			targets: [{ src: 'src/types/cast-shim.d.ts', dest: 'dist/types' }],
 		}),
+		addCastShimReference(), // Added custom plugin
 	],
 }
