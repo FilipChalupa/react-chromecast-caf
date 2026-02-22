8555b94f286a61f520ba7b572eb223eec2acb577
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-22 17:09:02 +0100

    Reset

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import fs from 'fs' // Added fs import
 import path from 'path'
 import copy from 'rollup-plugin-copy'
 import del from 'rollup-plugin-delete'
@@ -10,22 +9,6 @@ import packageJson from './package.json' with { type: 'json' }
 
 const outputDirectory = path.parse(packageJson.main).dir
 
-// Custom plugin to add reference to cast-shim.d.ts
-const addCastShimReference = () => ({
-	name: 'add-cast-shim-reference',
-	writeBundle: async () => {
-		const dtsPath = path.resolve(outputDirectory, 'index.d.ts')
-		const shimReference = '/// <reference path="./types/cast-shim.d.ts" />\n'
-
-		if (fs.existsSync(dtsPath)) {
-			const content = fs.readFileSync(dtsPath, 'utf8')
-			if (!content.startsWith(shimReference)) {
-				fs.writeFileSync(dtsPath, shimReference + content, 'utf8')
-			}
-		}
-	},
-})
-
 export default {
 	input: './src/index.ts',
 	output: {
@@ -33,11 +16,7 @@ export default {
 		format: 'esm',
 		sourcemap: true,
 	},
-	external: [
-		'react',
-		'chromecast-caf-receiver-module',
-		'chromecast-caf-sender-module',
-	],
+	external: ['react'],
 	plugins: [
 		del({ targets: outputDirectory + '/*' }),
 		peerDepsExternal(),
@@ -49,6 +28,5 @@ export default {
 		copy({
 			targets: [{ src: 'src/types/cast-shim.d.ts', dest: 'dist/types' }],
 		}),
-		addCastShimReference(), // Added custom plugin
 	],
 }
